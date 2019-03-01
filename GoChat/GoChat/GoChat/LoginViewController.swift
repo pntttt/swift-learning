660b40e74783672e138c572660b318294b61751d
pnt <ngocthach.pham@access-company.com>
2019-03-01 15:24:39 +0900

    init chat app

---
+++ GoChat/GoChat/GoChat/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//  GoChat
+//
+//  Created by Pham N. Thach on 2019/03/01.
+//  Copyright © 2019 pntttt. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func loginAnonymouslyDidTapped(_ sender: Any) {
+        print("ok")
+    }
+    @IBAction func googleLoginDidTapped(_ sender: Any) {
+    }
+}
